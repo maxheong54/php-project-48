8684c4addd42c1ab1a025694ee0678b2441456b9
maxheong54 <maxheong54@gmail.com>
2025-03-07 23:23:36 +0300

    Add genDiff function

--- Makefile
+++ Makefile
@@ -6,5 +6,6 @@ dump:
 	composer dump-autoload
 lint:
 	composer exec --verbose phpcs -- --standard=PSR12 src bin
+	composer exec --verbose phpstan
 lint-fix:
 	composer exec --verbose phpcbf -- --standard=PSR12 src bin
--- src/Differ.php
+++ src/Differ.php
@@ -2,8 +2,44 @@
 
 namespace Php\Project\Differ;
 
+use function Functional\sort;
+
+function genDiff(string $pathToFile1, string $pathToFile2): string
+{
+    $valueFile1 = parseJson($pathToFile1);
+    $valueFile2 = parseJson($pathToFile2);
+    $keys = sort(
+        array_unique([...array_keys($valueFile1), ...array_keys($valueFile2)]),
+        fn($left, $right): int => strcmp($left, $right)
+    );
+
+    $iter = function (array $acc, string $key) use ($valueFile1, $valueFile2): array {
+        $hasKey1 = array_key_exists($key, $valueFile1);
+        $hasKey2 = array_key_exists($key, $valueFile2);
+        $value1 = $hasKey1 ? var_export($valueFile1[$key], true) : null;
+        $value2 = $hasKey2 ? var_export($valueFile2[$key], true) : null;
+
+        if ($hasKey1 && $hasKey2 && $value1 === $value2) {
+            $acc[] = "    {$key}: {$value1}";
+            return $acc;
+        }
+        if ($hasKey1) {
+            $acc[] = "  - {$key}: {$value1}";
+        }
+        if ($hasKey2) {
+            $acc[] = "  + {$key}: {$value2}";
+        }
+
+        return $acc;
+    };
+
+    $result = array_reduce($keys, $iter, []);
+
+    return implode("\n", ['{', ...$result, '}']);
+}
+
 function parseJson(string $path): array
 {
     $jsonValue = file_get_contents($path);
-    return json_decode($jsonValue, true);
+    return ($jsonValue !== false) ? json_decode($jsonValue, true) : [];
 }
