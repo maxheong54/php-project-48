a71c643091eeb5715e7af603e8dc3e1d635dda26
maxheong54 <maxheong54@gmail.com>
2025-03-26 20:44:03 +0300

    Refactor Differ.php to reduce nesting

--- src/Differ.php
+++ src/Differ.php
@@ -27,36 +27,38 @@ function getDiff(array $valueFile1, array $valueFile2): array
     $keys = getKeys($valueFile1, $valueFile2);
 
     $iter = function (array $acc, string $key) use ($valueFile1, $valueFile2): array {
-        $hasKey1 = array_key_exists($key, $valueFile1);
-        $hasKey2 = array_key_exists($key, $valueFile2);
 
-        $value1 = $hasKey1 ? $valueFile1[$key] : null;
-        $value2 = $hasKey2 ? $valueFile2[$key] : null;
+        $value1 = $valueFile1[$key] ?? null;
+        $value2 = $valueFile2[$key] ?? null;
 
-        if ($hasKey1 && $hasKey2) {
-            if (is_array($value1) && is_array($value2)) {
-                $newValue = getDiff($value1, $value2);
-                return [...$acc, ['key' => $key, 'status' => 'compare', 'value' => $newValue]];
-            }
-            if ($value1 === $value2) {
-                return [...$acc, ['key' => $key, 'status' => 'unchanged', 'value' => $value1]];
-            } else {
-                return [
-                    ...$acc,
-                    [
-                        'key' => $key,
-                        'status' => 'updated',
-                        'value' => ['from' => $value1, 'to' => $value2],
-                    ],
-                ];
-            }
-        } elseif ($hasKey1) {
-            return [...$acc, ['key' => $key, 'status' => 'removed', 'value' => $value1]];
-        } elseif ($hasKey2) {
+        if (
+            (is_array($value1) && !array_is_list($value1)) &&
+            (is_array($value2) && !array_is_list($value2))
+        ) {
+            $newValue = getDiff($value1, $value2);
+            return [...$acc, ['key' => $key, 'status' => 'compare', 'value' => $newValue]];
+        }
+
+        if (!array_key_exists($key, $valueFile1)) {
             return [...$acc, ['key' => $key, 'status' => 'added', 'value' => $value2]];
         }
 
-        return $acc;
+        if (!array_key_exists($key, $valueFile2)) {
+            return [...$acc, ['key' => $key, 'status' => 'removed', 'value' => $value1]];
+        }
+
+        if ($value1 === $value2) {
+            return [...$acc, ['key' => $key, 'status' => 'unchanged', 'value' => $value1]];
+        }
+
+        return [
+            ...$acc,
+            [
+                'key' => $key,
+                'status' => 'updated',
+                'value' => ['from' => $value1, 'to' => $value2],
+            ],
+        ];
     };
 
     $result = array_reduce($keys, $iter, []);
